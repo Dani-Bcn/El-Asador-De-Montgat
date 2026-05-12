6cc24fc49fc81420c10fd3ed1d2a2929ef25d72a
Dani-Bcn <nneodani@gmail.com>
2026-05-12 21:17:01 +0200

    feat: add server.js file with Express setup and routes

--- backend/api/server.js
+++ backend/api/server.js
@@ -4,9 +4,9 @@ dotenv.config();
 import express from "express";
 import cors from "cors";
 
-import connectDB from "./config/db.js";
+import connectDB from "../src/config/db.js";
 
-import reservationRoutes from "./routes/reservationRoutes.js";
+import reservationRoutes from "../src/routes/reservationRoutes.js";
 
 
 
@@ -50,14 +50,3 @@ app.get("/", (req, res) => {
 
 
 
-/*
-|--------------------------------------------------------------------------
-| Server
-|--------------------------------------------------------------------------
-*/
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`🔥 Server running on port ${PORT}`);
-});
